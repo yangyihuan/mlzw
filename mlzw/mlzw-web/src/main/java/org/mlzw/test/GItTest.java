99117d54413939a77762031ced17f76acd2099d2
yangyihuan <18201310914@163.com>
2017-10-15 14:27:11 +0800

    Signed-off-by: yangyihuan <18201310914@163.com>

--- mlzw/mlzw-web/src/main/java/org/mlzw/test/GItTest.java
+++ mlzw/mlzw-web/src/main/java/org/mlzw/test/GItTest.java
@@ -3,7 +3,8 @@ package org.mlzw.test;
 public class GItTest {
 
 	public static void main(String[] args) {
-		System.out.println("test abcd");
+		System.out.println("test abcdfgv");
 		System.out.println("test git");
+		
 	}
 }
